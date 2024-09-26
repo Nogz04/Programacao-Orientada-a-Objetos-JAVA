ff9279f96527e15079af0e247e79fbd3c20e5334
Matheus Nogueira Albuquerque <127986753+Nogz04@users.noreply.github.com>
2024-09-25 22:40:09 -0300

    Update JavaBD01.java

--- javabd01.conexao/src/conexao/JavaBD01.java
+++ javabd01.conexao/src/conexao/JavaBD01.java
@@ -16,10 +16,10 @@ public class JavaBD01 {
     public static void main(String[] args) {
         
         //Instanciando a Classe Conexão que contém o método para conectar-se ao Banco de Dados (getConexao()).
-        //Conexao c = new Conexao();
+        Conexao c = new Conexao(); 
         
         //Chamando o método getConexao() que tem a função de conectar-se ao BD.
-        //c.getConexao();
+        c.getConexao();
         
         Pessoa p = new Pessoa();
         p.setNome("Enzo");
