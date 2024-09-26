108a342a6369753dd9d20c400e18f25a80ff49ff
Matheus Nogueira Albuquerque <127986753+Nogz04@users.noreply.github.com>
2024-09-25 22:34:35 -0300

    Add files via upload
    Programa em Java com objetivo de criar uma conexão com um Banco de Dados e possibilitar o CRUD através da IDE.

---
+++ javabd01.conexao/src/conexao/JavaBD01.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexao;
+
+import DAO.PessoaDAO;
+import beans.Pessoa;
+
+/**
+ *
+ * @author mathe
+ */
+public class JavaBD01 {
+    
+    public static void main(String[] args) {
+        
+        //Instanciando a Classe Conexão que contém o método para conectar-se ao Banco de Dados (getConexao()).
+        //Conexao c = new Conexao();
+        
+        //Chamando o método getConexao() que tem a função de conectar-se ao BD.
+        //c.getConexao();
+        
+        Pessoa p = new Pessoa();
+        p.setNome("Enzo");
+        p.setSexo("M");
+        p.setIdioma("Português");
+        
+        
+        PessoaDAO pDAO = new PessoaDAO();
+        pDAO.inserir(p);
+        
+    }
+    
+}
